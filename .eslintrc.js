095f4f8f8d86a89d43d0adc45b6b87ded96bbc6b
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-07-04 12:03:33 -0400

    Merge with working code and add linting

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+  },
+};
